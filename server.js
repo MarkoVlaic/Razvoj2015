ea73c0d7498054b41f8f243d92decf650083fcdf
MarkoVlaic <vlaic.marko@gmail.com>
2015-10-28 20:31:01 +0100

    Added my project and code

--- server.js
+++ server.js
@@ -68,9 +68,17 @@ app.use('/',addTask);
 app.use('/',upload);
 app.use('/',fileHandle);
 
-var port = process.env.port || 1337;
+var ipaddress =  process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 1337;
 
-app.listen(port,function(){
+if (typeof ipaddress === "undefined") {
+	//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+	//  allows us to run/test the app locally.
+	console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+	ipaddress = "127.0.0.1";
+}
+
+app.listen(port,ipaddress,function(){
   console.log("Server is listening on port",port);
 })
 
