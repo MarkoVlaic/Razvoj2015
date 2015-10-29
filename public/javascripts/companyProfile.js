a4befd93cd7840d4817f858e009c6b51b7e1eb8e
MarkoVlaic <vlaic.marko@gmail.com>
2015-10-29 22:52:36 +0100

    Dodao sam About us na profilu za tvrtke

--- public/javascripts/companyProfile.js
+++ public/javascripts/companyProfile.js
@@ -3,3 +3,11 @@ var companyProfile = angular.module('CompanyProfile',[]);
 companyProfile.controller('MenuController',function($scope){
 	
 });
+
+companyProfile.controller('AboutUsController',function($scope){
+	$scope.informations = [
+		{label:'description',value:"Not entered yet",type:"textarea",edit:false},
+		{label:'Address:',value:'Not entered yet',type:'text',edit:false},
+		
+	];
+});
