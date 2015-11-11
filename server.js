9bd361037dea3a8eca528eda3c897f903ffdc3e7
MarkoVlaic <vlaic.marko@gmail.com>
2015-11-11 13:18:42 +0100

    Baza

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //connect to the database
-var conn = mongoose.connect('mongodb://127.0.0.1/projektDb',function(){
+var conn = mongoose.connect('mongodb://osak:MaPa2015!@ds059722.mongolab.com:59722/osak',function(){
   console.log("database connected");
   // console.log("Bla bla",mongoose.connection.db);
   // console.log("Ko filip",mongoose.db);
